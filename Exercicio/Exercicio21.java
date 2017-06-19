28efbb0795b37031a673c27a4353c102c1dbafda
Guilherme Hiroshi Yoshii Caris <carishiroshi@gmail.com>
2017-06-19 20:40:43 -0300

    Update Exercicio21.java

--- Exercicio/Exercicio21.java
+++ Exercicio/Exercicio21.java
@@ -6,14 +6,17 @@ import java.util.Scanner;
 
 public class Exercicio21 {
     static int x;
-    public static void main(String[] args) {
-        System.out.println(tri(x));
+     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o numero");
+        x=entrada.nextInt();
+        
+     System.out.println(tri(x));
     }
     static int tri(int x){
-        Scanner entrada = new Scanner(System.in);
-        x=entrada.nextInt();
+        
         if (x == 1){
-            return 1;
-        }else {return x*(x+1)/2;}// formula do numero triamgulor n*(n-1)/n
+            return x;
+        }return x+tri(x-1);
     }
 }
